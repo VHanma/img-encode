bfb23801b4e5e984299daf92475051e2b3b261d1
VHanma <morgandeavon@gmail.com>
2026-08-29 11:53:37 -0500

    Bump JelliForge to v1.3

--- JelliForge/app/build.gradle.kts
+++ JelliForge/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.vhanma.jelliforge"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     signingConfigs {
