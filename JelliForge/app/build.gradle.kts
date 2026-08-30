a2810ddb374c31a61f77f674956aed8b48dd0f3e
VHanma <morgandeavon@gmail.com>
2026-08-30 17:55:49 -0500

    Version JelliForge 1.4

--- JelliForge/app/build.gradle.kts
+++ JelliForge/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.vhanma.jelliforge"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
     }
 
     signingConfigs {
